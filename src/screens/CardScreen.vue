316a7ed926799720411e4ba8a6755b110a891025
JoseCarlos-17 <josecarlosbsilva.172@gmail.com>
2025-06-16 15:23:50 -0300

    the card chosed component have been created

--- src/screens/CardScreen.vue
+++ src/screens/CardScreen.vue
@@ -1,50 +1,26 @@
 <template>
-  <div>
-    <el-row class="chosedCard" v-if="card">
-      <el-col :lg="6">
-        <div>
-          <el-button class="goBack" @click="backToTheCardList()">
-            Back to the list
-          </el-button>
-        </div>
-
-        <img :src="card.image_uris.png">
-      </el-col>
-
-      <el-col :xs="24" :sm="24" :md="24" :lg="12" class="card-description">
-        <h2><b>Name: </b>{{ card.name }}</h2>
-        <h3><b>Type: </b>{{ card.type_line }}</h3>
-
-        <p v-show="card.power && card.toughness">
-          <b>Power/Toughness: {{ card.power }}/{{ card.toughness }}</b>
-        </p>
-
-        <p v-show="card.oracle_text">
-          <b>Description: </b>{{ card.oracle_text }}
-        </p>
-
-        <p v-show="card.flavor_text">
-          <i>{{ card.flavor_text }}</i>
-        </p>
-      </el-col>
-    </el-row>
-  </div>
+  <CardSelectedComponent
+    :card="card"
+    :backToTheCardList="backToTheCardList"
+  />
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex'
+import CardSelectedComponent from '../components/CardSelectedComponent.vue'
 export default {
+  name: 'CardScreen',
+
+  components: {
+    CardSelectedComponent
+  },
+
   computed: {
-    ...mapState([
-      'card',
-      'search'
-    ])
+    ...mapState(['card', 'search'])
   },
 
   methods: {
-    ...mapActions([
-      'loadCard'
-    ]),
+    ...mapActions(['loadCard']),
 
     backToTheCardList () {
       this.$router.push({
