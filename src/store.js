94eceb2f4bfc64d82536934eb91fd946bf619aea
JoseCarlos-17 <josecarlosbsilva.172@gmail.com>
2025-08-12 00:29:22 -0300

    this bug was corrected

--- src/store.js
+++ src/store.js
@@ -7,8 +7,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     cardsList: [],
-    card: null,
-    search: ''
+    card: null
   },
 
   getters: {
@@ -18,10 +17,6 @@ export default new Vuex.Store({
 
     card (state) {
       return state.card
-    },
-
-    search (state) {
-      return state.search
     }
   },
 
@@ -32,10 +27,6 @@ export default new Vuex.Store({
 
     setCard (state, card) {
       state.card = card
-    },
-
-    setSearch (state, search) {
-      state.search = search
     }
   },
 
@@ -50,14 +41,6 @@ export default new Vuex.Store({
       return Card.getCard(payload).then(response => {
         commit('setCard', response.data)
       }).catch(error => error)
-    },
-
-    inputSearch ({ commit }, payload) {
-      commit('setSearch', payload)
-    },
-
-    cleanSearchBar ({ commit }) {
-      commit('setSearch', '')
     }
   }
 })
