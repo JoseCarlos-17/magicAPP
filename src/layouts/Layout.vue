9692e27886acf440f53c16bce25e467634d71fe8
JoseCarlos-17 <josecarlosbsilva.172@gmail.com>
2025-09-24 23:29:48 -0300

    search component finally is a dumb component

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,7 +1,12 @@
 <template>
   <el-row type="flex" justify="center">
     <el-col :xs="24" :sm="24" :md="24" :lg="24">
-      <Header />
+      <Header
+        :search="search"
+        @update_value="search = $event"
+        :searchingCard="searchingCard"
+      />
+
       <el-main>
         <transition name="fade" mode="out-in">
           <router-view />
@@ -15,11 +20,40 @@
 <script>
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   components: {
     Header,
     Footer
+  },
+
+  data () {
+    return {
+      search: ''
+    }
+  },
+
+  computed: {
+    ...mapState(['cardsList'])
+  },
+
+  methods: {
+    ...mapActions(['loadCardsList']),
+
+    searchingCard () {
+      if (this.$route.params.search) {
+        this.$router.push(
+          { name: 'cardslist', params: { search: this.search } }
+        )
+
+        this.loadCardsList(this.search)
+      } else {
+        this.$router.push(
+          { name: 'cardslist', params: { search: this.search } }
+        )
+      }
+    }
   }
 }
 </script>
